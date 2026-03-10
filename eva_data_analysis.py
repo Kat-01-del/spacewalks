d0a2c67733ddd68a53f02f904d09b031c8ed73c4
kat-ro <katharina.roettger@gmail.com>
2026-03-10 12:11:00 +0000

    add crew size analysis

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,6 +1,8 @@
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import re # added this line
 
 
 def main(input_file, output_file, graph_file):
@@ -8,6 +10,9 @@ def main(input_file, output_file, graph_file):
 
     # Read the data from JSON file
     eva_data = read_json_to_dataframe(input_file)
+
+    # Calculate and add crew size to data
+    eva_data = add_crew_size_column(eva_data) # added this line
 
     # Convert and export data to CSV file
     write_dataframe_to_csv(eva_data, output_file)
@@ -94,7 +99,7 @@ def text_to_duration(duration):
         duration_hours (float): The duration in hours
     """
     hours, minutes = duration.split(":")
-    duration_hours = int(hours) + int(minutes)/6  # there is an intentional bug on this line (should divide by 60 not 6)
+    duration_hours = int(hours) + int(minutes)/60  # there is an intentional bug on this line (should divide by 60 not 6)
     return duration_hours
 
 
@@ -114,17 +119,50 @@ def add_duration_hours(df):
     )
     return df_copy
 
+
+def calculate_crew_size(crew):
+    """
+    Calculate the size of the crew for a single crew entry
+
+    Args:
+        crew (str): The text entry in the crew column containing a list of crew member names
+
+    Returns:
+        (int): The crew size
+    """
+    if crew.split() == []:
+        return None
+    else:
+        return len(re.split(r';', crew))-1
+
+def add_crew_size_column(df):
+    """
+    Add crew_size column to the dataset containing the value of the crew size
+
+    Args:
+        df (pd.DataFrame): The input data frame.
+
+    Returns:
+        df_copy (pd.DataFrame): A copy of the dataframe df with the new crew_size variable added
+    """
+    print('Adding crew size variable (crew_size) to dataset')
+    df_copy = df.copy()
+    df_copy["crew_size"] = df_copy["crew"].apply(
+        calculate_crew_size
+    )
+    return df_copy
+    
+
 if __name__ == "__main__":
 
     if len(sys.argv) < 3:
         input_file = 'data/eva-data.json'
         output_file = 'results/eva-data.csv'
-        print(f'Using default input and output filenames')
+        print(f'Using default file paths and filenames: {input_file}, {output_file}')
     else:
         input_file = sys.argv[1]
         output_file = sys.argv[2]
-        print('Using custom input and output filenames')
-
-        graph_file = 'results/cumulative_eva_graph.png'
-
-        main(input_file, output_file, graph_file)
+    
+    graph_file = 'results/cumulative_eva_graph.png'
+    main(input_file, output_file, graph_file)    
+    
